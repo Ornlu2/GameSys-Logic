066f8d6a6bd1288d49fe6c8b9959862fdcfbb420
Dylan <alterdylan@gmail.com>
2018-02-26 12:37:37 -0500

    class fixes
    possible additions by slvy:
    have a neutral color on outside walls that automatically switches color to your player if you get something on the inside
    
    change maps size(current is biggest)
    
    more player feedback for turn
    
    add more polish and main menu for more points

---
+++ GameSysLogic/Assets/Scripts/Triangle.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triangle : MonoBehaviour {
+
+    private GameManager GM;
+
+    public bool P1Captured;
+    public bool P2Captured;
+    private SpriteRenderer TrianglePiece;
+
+    private Color OriginalColor;
+
+    // Use this for initialization
+    void Start () {
+        GM = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+
+        TrianglePiece = gameObject.GetComponent<SpriteRenderer>();
+
+        OriginalColor = TrianglePiece.color;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+       
+	}
+    private void OnMouseOver()
+    {
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null)
+            {
+                // Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+            }
+            if (GM.Player1Turn == true)
+            {
+
+
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(0, 0, 255);
+
+
+                if (Input.GetMouseButtonUp(1))
+                {
+
+                    if ( P2Captured == false)
+                    {
+                        P2Captured = true;
+                        P1Captured = false;
+                        Player1Captured();
+
+                    }
+                    else if (P1Captured == true)
+                    {
+                        return;
+                    }
+                    else if (P2Captured ==true)
+                    {
+                        GM.P1Score++;
+                        GM.P2Score--;
+                    }
+
+                }
+
+
+
+            }
+            if (GM.Player2Turn == true)
+            {
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(255, 0, 0);
+
+
+                if (Input.GetMouseButtonUp(1))
+                {
+
+                    if (P1Captured == false )
+                    {
+                        P2Captured = true;
+                        P1Captured = false;
+                        Player2Captured();
+                    }
+                    else if(P2Captured ==true)
+                    {
+                        return;
+                    }
+                    else if (P1Captured == true)
+                    {
+                        GM.P2Score++;
+                        GM.P1Score--;
+                    }
+
+                }
+
+
+
+            }
+        }
+    }
+    private void OnMouseExit()
+    {
+        /*
+        if (P2Captured == true&& P1Captured ==false)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(255, 0, 0);
+            Debug.Log("Mouse leaving space blue");
+
+        }
+       else  if (P1Captured ==true&& P2Captured ==false)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(0, 0, 255);
+            Debug.Log("Mouse leaving space red");
+
+        }
+        */
+         
+         if (P1Captured == false && P2Captured == false)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = OriginalColor;
+        }
+    }
+    void Player1Captured()
+    {
+        Debug.Log("Blue Captured");
+        GM.P1Score++;
+    }
+    void Player2Captured()
+    {
+        Debug.Log("Red Captured");
+        GM.P2Score++;
+    }
+}
---
+++ GameSysLogic/Assets/Scripts/TriangleSegment.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriangleSegment : MonoBehaviour {
+
+    public bool P1Captured;
+    public bool P2Captured;
+
+    private GameManager GM;
+    private SpriteRenderer TriangleSeg;
+    private Color OriginalColor;
+
+    // Use this for initialization
+    void Start () {
+        GM = GameObject.Find("GameManager").GetComponent<GameManager>();
+        //Fetch the mesh renderer component from the GameObject
+        TriangleSeg = GetComponent<SpriteRenderer>();
+        
+        //Fetch the original color of the GameObject
+        OriginalColor = TriangleSeg.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+    private void OnMouseOver()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (hit.collider != null)
+        {
+           // Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+        }
+        if (GM.Player1Turn == true)
+        {
+            if(P2Captured==false)
+            {
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(0, 0, 255);
+
+            }
+
+            if (Input.GetMouseButtonUp(0)&& P2Captured == false&& P1Captured == false)
+            {
+                Player1Captured();
+                P1Captured = true;
+            }
+          
+
+                    
+        }
+        if (GM.Player2Turn == true)
+        {
+            if (P1Captured == false)
+            {
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(255, 0, 0);
+            }
+            if (Input.GetMouseButtonUp(0)&& P1Captured ==false&& P2Captured ==false)
+            {
+                Player2Captured();
+                P2Captured = true;
+            }
+            
+
+
+        }
+    }
+    private void OnMouseExit()
+    {
+        if (P1Captured == false && P2Captured == false)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = OriginalColor;
+        }
+    }
+    void Player1Captured()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(0, 0, 255);
+        Debug.Log("Blue Captured");
+        GM.ChangePlayer();
+    }
+    void Player2Captured()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(255, 0, 0);
+        Debug.Log("Red Captured");
+        GM.ChangePlayer();
+    }
+
+}
